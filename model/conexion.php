238bddc16070b6f04040b4be0b0536c1d32d0ee9
Arturo Alejandro C <arthurogoldenon@gmail.com>
2021-05-19 15:46:29 -0500

    asiste.nextcam C11

--- model/conexion.php
+++ model/conexion.php
@@ -44,6 +44,48 @@
         mysqli_query($conexion, "SET NAMES utf8");
     }
 
+    function procesoRespaldo($yesterdayDate, $remoteConexion){
+        
+        global $conexion;
+
+        date_default_timezone_set('America/Mexico_City');
+        $horaActual= date("H");
+
+        if(intval($horaActual) > 8 && intval($horaActual) <= 23){
+            $remoteQuery= "SELECT COUNT(*) AS SI_HAY FROM asistencia WHERE fecha='$yesterdayDate' ORDER BY idasistencia DESC";
+            
+            $result= mysqli_query($remoteConexion, $remoteQuery);
+            $row= mysqli_fetch_array($result);
+            
+            // Si no hay registros del dia anterior en la bd remota. Transferir todos los registros..
+            if(intval($row['SI_HAY']) == 0){
+                // Realizar una consulta con todas las asistencias del dia anterior en la bd local.
+                $localQuery= "SELECT * FROM asistencia WHERE fecha='$yesterdayDate'";
+                $result2= mysqli_query($conexion, $localQuery);
+                
+                // Si hay registros en la base de datos local..
+                if(!empty($localRows)){
+                    $allRight= true;
+                    while($lr= mysqli_fetch_array($result2)){
+                        $remoteQuery= "INSERT INTO asistencia (codigo_persona, fecha_hora, tipo, fecha) VALUES ('$lr[codigo_persona]','$lr[fecha_hora]','$lr[tipo]','$lr[fecha]')";
+                        if(!(mysqli_query($remoteConexion, $remoteQuery))){
+                            $allRight= false;
+                            break;
+                        }
+                    }
+
+                    if(!$allRight){
+                        die("Hubo un error al transferir una asistencia local a la base de datos remota.. query => ".$remoteQuery." mysql_error => ".mysqli_error($remoteConexion));
+                    }
+                }else{
+                    exit;
+                }
+            }
+            
+            mysqli_close($remoteConexion);
+        }
+    }
+
     function respaldarAsistencias(){
         global $localHost;
 
@@ -52,7 +94,6 @@
 
             if($conexion != null || !empty($conexion)){
                 date_default_timezone_set('America/Mexico_City');
-                $horaActual= date("H");
                 
                 /**
                  *  Variables Remotas: Base de Datos
@@ -65,82 +106,17 @@
 
                 $remoteConexion= new mysqli($remoteHost, $remoteUser, $remotePass, $remoteDB);
 
-                //$remoteConexion= $conexion;
                 if($remoteConexion != null || !empty($remoteConexion)){
 
                     // Pregunto si el dia actual es lunes
                     if(strtolower(date("l")) == "monday"){
+                        // Hacer el respaldo del sábado
                         $yesterdayDate = date('Y-m-d',strtotime("-2 days"));
-
-                        if(intval($horaActual) > 8 && intval($horaActual) <= 23){
-                            $remoteQuery= "SELECT COUNT(*) AS SI_HAY FROM asistencia WHERE fecha='$yesterdayDate' ORDER BY idasistencia DESC";
-                            
-                            $result= mysqli_query($remoteConexion, $remoteQuery);
-                            $row= mysqli_fetch_array($result);
-                            
-                            // Si no hay registros del dia anterior en la bd remota. Transferir todos los registros..
-                            if(intval($row['SI_HAY']) == 0){
-                                // Realizar una consulta con todas las asistencias del dia anterior en la bd local.
-                                $localQuery= "SELECT * FROM asistencia WHERE fecha='$yesterdayDate'";
-                                $result2= mysqli_query($conexion, $localQuery);
-                                
-                                // Si hay registros en la base de datos local..
-                                if(!empty($localRows)){
-                                    $allRight= true;
-                                    while($lr= mysqli_fetch_array($result2)){
-                                        $remoteQuery= "INSERT INTO asistencia (codigo_persona, fecha_hora, tipo, fecha) VALUES ('$lr[codigo_persona]','$lr[fecha_hora]','$lr[tipo]','$lr[fecha]')";
-                                        if(!(mysqli_query($remoteConexion, $remoteQuery))){
-                                            $allRight= false;
-                                            break;
-                                        }
-                                    }
-    
-                                    if(!$allRight){
-                                        die("Hubo un error al transferir una asistencia local a la base de datos remota.. query => ".$remoteQuery." mysql_error => ".mysqli_error($remoteConexion));
-                                    }
-                                }else{
-                                    exit;
-                                }
-                            }
-                            
-                            mysqli_close($remoteConexion);
-                        }
-                    }
-
-                    $yesterdayDate = date('Y-m-d',strtotime("-1 days"));
-
-                    if(intval($horaActual) > 8 && intval($horaActual) <= 23){
-                        $remoteQuery= "SELECT COUNT(*) AS SI_HAY FROM asistencia WHERE fecha='$yesterdayDate' ORDER BY idasistencia DESC";
-                        
-                        $result= mysqli_query($remoteConexion, $remoteQuery);
-                        $row= mysqli_fetch_array($result);
-                        
-                        // Si no hay registros del dia anterior en la bd remota. Transferir todos los registros..
-                        if(intval($row['SI_HAY']) == 0){
-                            // Realizar una consulta con todas las asistencias del dia anterior en la bd local.
-                            $localQuery= "SELECT * FROM asistencia WHERE fecha='$yesterdayDate'";
-                            $result2= mysqli_query($conexion, $localQuery);
-                            
-                            // Si hay registros en la base de datos local..
-                            if(!empty($localRows)){
-                                $allRight= true;
-                                while($lr= mysqli_fetch_array($result2)){
-                                    $remoteQuery= "INSERT INTO asistencia (codigo_persona, fecha_hora, tipo, fecha) VALUES ('$lr[codigo_persona]','$lr[fecha_hora]','$lr[tipo]','$lr[fecha]')";
-                                    if(!(mysqli_query($remoteConexion, $remoteQuery))){
-                                        $allRight= false;
-                                        break;
-                                    }
-                                }
-
-                                if(!$allRight){
-                                    die("Hubo un error al transferir una asistencia local a la base de datos remota.. query => ".$remoteQuery." mysql_error => ".mysqli_error($remoteConexion));
-                                }
-                            }else{
-                                exit;
-                            }
-                        }
-                        
-                        mysqli_close($remoteConexion);
+                        procesoRespaldo($yesterdayDate, $remoteConexion);
+                    }else{
+                        // Hacer el respaldo del día anterior
+                        $yesterdayDate = date('Y-m-d',strtotime("-1 days"));
+                        procesoRespaldo($yesterdayDate, $remoteConexion);
                     }
                 }else{
                     die("No se pudo establecer conexion con la base de datos remota..");
